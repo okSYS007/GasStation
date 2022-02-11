9f33d1c18f5590c43ccb74d30c56ce39c5e40fa7
okSYS <ViacheslavDan803@gmail.com>
2022-02-11 21:17:40 +0200

    auto

--- authorization.py
+++ authorization.py
@@ -1,8 +1,36 @@
 
-
 def check_auto():
+
     f = open('DataBase/db.csv', 'w')
-    f.write('Slava - 123')
+    f.write('Slava:123')
     f.close()
 
-check_auto()
+def getUser(login, password):
+    global message
+    with open('DataBase/db.csv') as f:
+        for line in f:
+            arr = line.strip().split(':')
+            if login in arr and password in arr:
+                message = 'Добро пожаловать,'
+                return True    
+            if login in arr:
+                message = f'Неверный пароль'
+                return False
+            else:
+                message = f'Пользователь {login} не найден'
+                return False
+    
+def autorizate():
+   
+    while True:
+        login = input('Введите логин:')
+        password = input('Введите пароль:')
+        if getUser(login, password):
+            print(f'{message} {login}')
+            break
+        else:
+            print(message)
+                
+message = ''
+
+autorizate()
